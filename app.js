59d1b6c25878d8f9bee001d600b6608d3433b8f2
Logan Allen <loganpat.allen@gmail.com>
2017-11-09 15:27:18 +0000

    checkpoint-8-dom-app-ui: completed

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
+let id = 0;
 function onReady(){
-    const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
-    addToDoForm.addEventListener('submit' , () => {
-      event.preventDefault();
-      let title = newToDoText.value;
-      let newLi =document.createElement('li');
-      let checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
-      newLi.textContent = title;
-      newLi.appendChild(checkbox);
-      toDoList.appendChild(newLi);
-      newToDoText.value = '';
+  const toDos = [];
+  const addToDoForm = document.getElementByID('addToDoForm');
+  function createNewToDo(){
+    const newToDoText = document.getElementByID('newToDoText');
+    if (!newToDoText.value) { return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id++
     });
+    newToDoText.value = '';
+    renderTheUI();
+  }
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const deleteButton = document.createElement('button');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      newLi.textContent = toDo.title;
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+      
+    deleteButton.addEventListener('onclick' , event =>{
+      toDos.filter(toDos => toDos.id != id);
+  });
+    renderTheUI();
+      
+    });
+  }
+  addToDoForm.addEventListener('submit' , event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+  
+  renderTheUI();
 }
 window.onload = function() {
     onReady();
